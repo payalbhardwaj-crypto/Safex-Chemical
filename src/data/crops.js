b6674b1740290eae8d505fe0e445602548de989b
Payal342 <paayal779@gmail.com>
2026-04-01 15:17:19 +0530

    feat: add products and crops data

---
+++ src/data/crops.js
@@ -0,0 +1,44 @@
+export const crops = [
+  {
+    id: 'rice',
+    name: 'Rice',
+    icon: '🌾',
+    productIds: ['girdle', 'nato', 'hallabol'],
+  },
+  {
+    id: 'wheat',
+    name: 'Wheat',
+    icon: '🌿',
+    productIds: ['cymate', 'dabang', 'bahubali'],
+  },
+  {
+    id: 'cotton',
+    name: 'Cotton',
+    icon: '🌱',
+    productIds: ['acarifas', 'nato', 'fipro-ultra'],
+  },
+  {
+    id: 'sugarcane',
+    name: 'Sugarcane',
+    icon: '🎋',
+    productIds: ['goodbye', 'girdle', 'bahaar'],
+  },
+  {
+    id: 'vegetables',
+    name: 'Vegetables',
+    icon: '🥦',
+    productIds: ['acarifas', 'cymate', 'dabang'],
+  },
+  {
+    id: 'fruits',
+    name: 'Fruits',
+    icon: '🍊',
+    productIds: ['acarifas', 'hallabol', 'bahubali'],
+  },
+  {
+    id: 'pulses',
+    name: 'Pulses',
+    icon: '🫘',
+    productIds: ['fipro-ultra', 'cymate', 'bahaar'],
+  },
+]
